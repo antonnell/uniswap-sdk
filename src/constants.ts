9e69e604c3725b8f94cb850b792d150bbac994da
antonnell <antonnell.crpto@gmail.com>
2021-03-03 12:42:52 +0200

    Using FANTOM factory Added 250 to chainID Added FTM as currency export Create a WETH (FETH) for chain 250
    Signed-off-by: antonnell <antonnell.crpto@gmail.com>

--- src/constants.ts
+++ src/constants.ts
@@ -8,7 +8,8 @@ export enum ChainId {
   ROPSTEN = 3,
   RINKEBY = 4,
   GÖRLI = 5,
-  KOVAN = 42
+  KOVAN = 42,
+  FANTOM = 250
 }
 
 export enum TradeType {
@@ -22,7 +23,7 @@ export enum Rounding {
   ROUND_UP
 }
 
-export const FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+export const FACTORY_ADDRESS = '0xc35DADB65012eC5796536bD9864eD8773aBc74C4'
 
 export const INIT_CODE_HASH = '0x96e8ac4277198ff8b6f785478aa9a39f403cb768dd02cbee326c3e7da348845f'
 
